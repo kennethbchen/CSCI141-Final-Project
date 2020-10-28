01b4abc99d8d3e7c856f56567025a147239e6a10
kennethbchen <kennethbchen@gmail.com>
2020-10-28 18:36:54 -0400

    Changed layout

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import game.GBUtility;
 
 import javax.swing.*;
 
-public class Main extends JFrame{
+public class Main extends JFrame {
 
     JPanel display;
+
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
@@ -33,15 +34,26 @@ public class Main extends JFrame{
         GBUtility.addGBComponent(this, leftPad, 0, 0, 1, 3, GridBagConstraints.BOTH, GridBagConstraints.CENTER, .10, 0.0, 0, 0, 0, 0);
 
         display = new JPanel();
-        display.setBackground(Color.BLACK);
+        display.setBackground(Color.WHITE);
         GBUtility.addGBComponent(this, display, 1, 0, 4, 2, GridBagConstraints.BOTH, GridBagConstraints.CENTER, 1, 1, 5, 0, 0, 0);
 
         JPanel rightPad = new JPanel();
         //rightPad.setBackground(Color.RED);
         GBUtility.addGBComponent(this, rightPad, 5, 0, 1, 3, GridBagConstraints.BOTH, GridBagConstraints.CENTER, .10, 0.0, 0, 0, 0, 0);
         
-        GBUtility.addGBComponent(this, new JLabel("Health"), 1, 2, 2, 1, GridBagConstraints.NONE, GridBagConstraints.CENTER, 0.66, 0.20, 0, 0, 0, 0);
-        GBUtility.addGBComponent(this, new JLabel("Attack"), 3, 2, 1, 1, GridBagConstraints.NONE, GridBagConstraints.CENTER, 0.165, 0.20, 0, 0, 0, 0);
+        JPanel healthContainer = new JPanel();
+        healthContainer.setLayout(new BoxLayout(healthContainer, BoxLayout.Y_AXIS));
+        healthContainer.add(new JLabel("Health"));
+        healthContainer.add(new JLabel("[Health Bar]"));
+        GBUtility.addGBComponent(this, healthContainer, 1, 2, 2, 1, GridBagConstraints.NONE, GridBagConstraints.CENTER, 0.66, 0.20, 0, 0, 0, 0);
+
+        JPanel statsContainer = new JPanel();
+        statsContainer.setLayout(new BoxLayout(statsContainer, BoxLayout.Y_AXIS));
+        statsContainer.add(new JLabel("Health:"));
+        statsContainer.add(new JLabel("Defense:"));
+        GBUtility.addGBComponent(this, statsContainer, 3, 2, 1, 1, GridBagConstraints.NONE, GridBagConstraints.CENTER, 0.165, 0.20, 0, 0, 0, 0);
+
+        
         GBUtility.addGBComponent(this, new JLabel("Keys"), 4, 2, 1, 1, GridBagConstraints.NONE, GridBagConstraints.CENTER, 0.165, 0.20, 0, 0, 0, 0);
 
         
